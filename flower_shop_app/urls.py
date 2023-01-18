ee816272c601c6a72606825a47d5e7e5138da35f
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-01-18 16:55:54 +0300

    Added index page

---
+++ flower_shop_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from flower_shop_app import views
+
+app_name = 'flower_shop_app'
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
--- flower_shop_app/views.py
+++ flower_shop_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from flower_shop_app.models import FlowerBouquet
+
+
+def index(request):
+    bouquets = FlowerBouquet.objects.filter(availability=True).order_by('-created')[:3]
+    return render(
+        request,
+        'flower_shop_app/index.html',
+        context={
+            'recommended_bouquets': bouquets,
+        }
+    )
