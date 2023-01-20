aed108fda864d90b47819ba665106703e1ee008c
pkzrnvch <pkazarnovich@gmail.com>
2023-01-20 22:26:10 +0500

    Add form for order details

--- flower_shop_app/forms.py
+++ flower_shop_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from flower_shop_app.models import ConsultationRequest
+from flower_shop_app.models import ConsultationRequest, Order
 
 
 class ConsultationRequestForm(forms.ModelForm):
@@ -27,3 +27,34 @@ class ConsultationRequestForm(forms.ModelForm):
         if not self.cleaned_data['acceptance_of_rules']:
             raise forms.ValidationError('Вы должны согласиться с правилами')
         return self.cleaned_data['acceptance_of_rules']
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ['client_name', 'phone_number', 'address', 'delivery_time']
+        widgets = {
+            'delivery_time': forms.RadioSelect(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['client_name'].widget.attrs.update({
+            'class': 'order__form_input',
+            'placeholder': 'Введите Имя',
+        })
+        self.fields['client_name'].label = ""
+        self.fields['phone_number'].widget.attrs.update({
+            'class': 'order__form_input',
+            'placeholder': '+ 7 (999) 000 00 00',
+        })
+        self.fields['phone_number'].label = ""
+        self.fields['address'].widget.attrs.update({
+            'class': 'order__form_input',
+            'placeholder': 'Адрес доставки',
+        })
+        self.fields['address'].label = ""
+        self.fields['delivery_time'].widget.attrs.update({
+            'class': 'order__form_radio',
+        })
